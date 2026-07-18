e9926e49621dc063301717acb50363bab70dd816
flash <iamflashuwu@gmail.com>
2026-07-19 05:25:25 +0800

    Fixed the issue of  the feature of auto change server.

--- resource/main/dist/client-bundle.js
+++ resource/main/dist/client-bundle.js
@@ -6278,6 +6278,8 @@
                       setTimeout(function () {
                         window.location.reload();
                       }, 250));
+                  }  else if ("cancel" === btnName){
+                    _serverDD.value = Network_1.Network.connectedServerIndex.toString();
                   }
                   promptWnd.close();
                 },
@@ -25174,14 +25176,17 @@
         let isForcingServer = parseInt(_forcedServerIndex) in servers_1.SERVERS;
         window.localStorage.setItem("chooseServerOnStart", "-1");
 
-        for (var _forcedServerIndexInt = null !== _forcedServerIndex ? parseInt(_forcedServerIndex) : -1, arr = window.location.search.substr(1).split("&"), _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
-            var el = arr_1[_i],
-                vals = el.split("=");
-
-            "server" === vals[0] && !isForcingServer && (_forcedServerIndexInt = parseInt(vals[1]));
-            "game" === vals[0] && (_forcedGameID = parseInt(vals[1]));
-
-        }
+        var lastQueryString = window.location.href.split('?').pop();
+        var _forcedServerIndexInt = null !== _forcedServerIndex ? parseInt(_forcedServerIndex) : -1;
+        var _forcedGameID = -1;
+        
+        lastQueryString.split('&').forEach(function(p) {
+            var v = p.split('=');
+            if (v.length === 2) {
+                "server" === v[0] && !isForcingServer && (_forcedServerIndexInt = parseInt(v[1]));
+                "game" === v[0] && (_forcedGameID = parseInt(v[1]));
+            }
+        });
         function networkInit() {
           for (var i = 0; i < servers_1.SERVERS.length; i++)
             if (-1 === _forcedServerIndexInt || _forcedServerIndexInt === i)
